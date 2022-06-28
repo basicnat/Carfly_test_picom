7899e76123676230d985e312f6f186903043b6c1
basicnat <na-2sa@yandex.ru>
2022-06-29 00:31:08 +0400

    tabs

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello!")
+let tab = function () {
+    let tabNav = document.querySelectorAll('.tab-nav_elem'),
+        tabDesc = document.querySelectorAll('.tab-desc_elem'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+        tabName = this.getAttribute('id');
+        selectTabDesc(tabName);
+    }
+
+    function selectTabDesc(tabName) {
+        tabDesc.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('active') : item.classList.remove('active');
+        })
+    }
+};
+
+tab();
